d5601528d77b0d539971196f865a8ddb90b15cda
Sailesh Patnala <sailesh.patnala.96@gmail.com>
2017-10-02 10:33:11 -0700

    added static and templates directories for Flask

--- server.py
+++ server.py
@@ -4,8 +4,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def root():
+def home():
 	return render_template('index.html')
+
+@app.route('/projects')
+def projects():
+	return render_template('projects.html')
 
 if __name__ == '__main__':
 	app.run()
